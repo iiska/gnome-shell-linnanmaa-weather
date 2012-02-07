58cbcd9edd9ad62194897c132a0a247e80102617
Juhamatti Niemelä <iiska@iki.fi>
2012-02-07 13:51:34 +0200

    Retrieve XML data

--- linnanmaa-weather@iiska.kapsi.fi/extension.js
+++ linnanmaa-weather@iiska.kapsi.fi/extension.js
@@ -1,33 +1,35 @@
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
+const Mainloop = imports.mainloop;
+const Soup = imports.gi.Soup;
+
+const _httpSession = new Soup.SessionAsync();
+Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefault());
 
 let text, button;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
+function _getData(callback) {
+    var request = Soup.Message.new('GET', 'http://weather.willab.fi/weather.xml');
+
+    _httpSession.queue_message(request, function(_httpSession, message) {
+      if (message.status_code !== 200) {
+        callback(message.status_code, null);
+        return;
+      }
+      callback(null, request);
+    });
 }
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
+function _updateData() {
+    _getData(function(s,r) {
+        let response = r.response_body.data.replace(/^<\?xml\s+version\s*=\s*(["'])[^\1]+\1[^?]*\?>/, "");
+        let data = new XML(response);
+        text.set_text(data.weather.tempnow + data.weather.tempnow.@unit);
+    });
+}
 
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
-                      Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
+function _showDetails() {
 }
 
 function init() {
@@ -37,18 +39,31 @@ function init() {
                           x_fill: true,
                           y_fill: false,
                           track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run',
+
+    let hbox = new St.BoxLayout({name: 'weather-button-area'});
+    let icon = new St.Icon({ icon_name: 'weather-few-clouds',
                              icon_type: St.IconType.SYMBOLIC,
                              style_class: 'system-status-icon' });
+    text = new St.Label({text: "-- C"});
+    _updateData();
+    hbox.add(icon);
+    hbox.add(text);
 
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
+    button.set_child(hbox);
+    button.connect('button-press-event', _updateData);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_actor(button, 0);
+    Main.panel._centerBox.insert_actor(button, 0);
+    _updateData();
+
+    //Mainloop.timeout_add_seconds(1800, function() {
+    //    _updateData();
+    //});
+    //Mainloop.run('updatedata');
 }
 
 function disable() {
-    Main.panel._rightBox.remove_actor(button);
+    //Mainloop.quit('updatedata');
+    Main.panel._centerBox.remove_actor(button);
 }
